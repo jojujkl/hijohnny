26475e1a5c435374532fd4a269bab93cd856d779
jojujkl <62133563+jojujkl@users.noreply.github.com>
2020-05-01 15:52:29 +0800

    Update views.py

--- studentsapp/views.py
+++ studentsapp/views.py
@@ -70,7 +70,7 @@ def hello3 (request,username):
 
 def index(request):
                 now=datetime.now()
-                username="daphne lo" 
+                username="劉家鳴" 
                 return render(request,"hello4.html",locals())
 
 def listone(request): 
